15abdefd7202b2de4c34299e44dc9f5b24962f8d
Eva <evaviebie@gmail.com>
2023-12-06 12:28:13 +0100

    Peilgebied class aangepast, Readme aangepast

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # data_modellen_polder
+
+## vertalingen Engels - Nederlands
+|Engels | Nederlands|
+|-------|-----------|
+|Gauge area | Peilgebied|
+|Open water | Open water|
+|Paved | Verhard |
+|Unpaved | Onverhard |
---
+++ classes/peilgebied.py
@@ -0,0 +1,6 @@
+class Gauge_area():
+    def __init__(self, area, per_openwater, per_paved, per_unpaved):
+        self.area = area
+        self.per_openwater = per_openwater
+        self.per_paved = per_paved
+        self.per_unpaved = per_unpaved
