677fb762a397d8fdb8e10dbcf26890ca13c9e3be
Eva <evaviebie@gmail.com>
2023-12-06 15:02:24 +0100

    waterpeil toegevoegd

--- classes/gauge_area.py
+++ classes/gauge_area.py
@@ -7,6 +7,8 @@ class Gauge_area():
         self.per_unpaved = per_unpaved
 
         self.neighbours = {}
+        self.volume = None
+        self.waterlevel = None
 
     def add_neighbours(self, gauge_area):
         """Add neighbours to the gauge_area"""
@@ -17,6 +19,7 @@ class Gauge_area():
         return self.neighbours
     
     def remove_neighbour(self, gauge_area):
+        """Remove neighbour from neighbours"""
         
         if gauge_area.area_id in self.neighbours.keys():
             self.neighbours.pop(gauge_area.area_id)
@@ -29,11 +32,18 @@ class Gauge_area():
     
     def get_area_unpaved(self):
         return (self.per_unpaved / 100) * self.area
+
+    def set_waterlevel(self, waterlevel):
+        self.waterlevel = waterlevel
+    
+    
         
     def __repr__(self):
         return f'(gauge_area_id: {self.area_id})'
 
 
+
+#Test code
 gauge_area_1 = Gauge_area(1, 20, 30, 50, 20) 
 gauge_area_2 = Gauge_area(2, 20, 30, 50, 20)
 gauge_area_3 = Gauge_area(3, 20, 30, 50, 20)
@@ -51,3 +61,5 @@ print(f"open water: {gauge_area_1.get_area_openwater()}")
 print(f"verhard: {gauge_area_1.get_area_paved()}")
 print(f"onverhard: {gauge_area_1.get_area_unpaved()}")
 
+gauge_area_1.set_waterlevel(5)
+print(gauge_area_1.waterlevel)
