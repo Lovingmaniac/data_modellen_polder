8daad148d238ef43a37dec0ee732346a92714adb
Lenovo Ideapad 5 Eva <evaviebie@gmail.com>
2024-01-13 17:23:36 +0100

    streamlit dingen

--- classes/streamlit_test.py
+++ classes/streamlit_test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
 
 #Defenition that contains all the necessary information for page 1
 def page_one():
