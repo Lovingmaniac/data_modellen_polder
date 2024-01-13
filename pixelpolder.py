2e85f000e15b4a6eec47b021fe4349782253551b
Lenovo Ideapad 5 Eva <evaviebie@gmail.com>
2024-01-13 17:07:16 +0100

    streamlit dingen

--- pixelpolder.py
+++ pixelpolder.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
 
 #Defenition that contains all the necessary information for page 1
 def page_one():
